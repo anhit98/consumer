106fb9ca96bbed8b849c1881a1d8ca6db0a9c589
anhit98 <34483809+anhit98@users.noreply.github.com>
2019-06-03 19:37:08 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,40 @@
+'use strict'; 
+const Hapi = require('hapi'); 
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./../package');
+require('dotenv').config()
+const port=process.env.PORT || 3000
+const server = Hapi.server({ port: port, host: process.env.HOST}); 
+
+const init = async () => {
+    
+    const swaggerOptions = {
+            info: {
+                    title: 'Pizza Processor API Documentation',
+                    version: Pack.version,
+                },
+            };
+        
+        await server.register([
+            Inert,
+            Vision,
+            {
+                plugin: HapiSwagger,
+                options: swaggerOptions
+            }
+        ]);
+        await server.start(); 
+        console.log(`Server running at: ${server.info.uri}`); 
+        require("./../src/services/consumer");
+        // server.route(require('./../src/routes/process'));
+}; 
+
+init().catch(err => {
+    console.error('An error happened while initializing the server', {
+        err,
+    });
+    process.exit(1);
+});
+module.exports = server;
---
+++ src/services/consumer.js
@@ -0,0 +1,39 @@
+const Kafka = require("node-rdkafka");
+const Joi = require('joi');
+require('dotenv').config();
+var kafkaConf = {
+  "group.id": "cloudkarafka-example",
+  "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
+  "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
+  "debug": "generic,broker,security",
+  "enable.partition.eof": false
+};
+const prefix = process.env.CLOUDKARAFKA_USERNAME;
+const topics = [`${prefix}.updateStatus`];
+const consumer = new Kafka.KafkaConsumer(kafkaConf);
+// console.log("fgrdfgt");
+// Flowing mode
+
+// Flowing mode
+consumer.connect();
+
+
+consumer
+  .on('ready', function() {
+    // Subscribe to the librdtesting-01 topic
+    // This makes subsequent consumes read from that topic.
+    consumer.subscribe(['update']);
+    console.log("grdf");
+    // Read one message every 1000 milliseconds
+    setInterval(function() {
+      consumer.consume(1);
+    }, 1000);
+  })
+  consumer.consume( function(data) {
+    console.log('Message found!  Contents below.');
+    console.log(data.value.toString());
+  });
